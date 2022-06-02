2bc41686fb806aceb5ec76cf9d44e003834a9063
Thomas Castaldi <tomas2mars@hotmail.fr>
2022-06-02 16:41:19 +0200

    Changing BG color

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-const navbar = document.getElementsByClassName('navbar');
+function changeBg() {
+  var navbar = document.getElementById('nav');
+  var scrollValue = window.scrollY;
+  if (scrollValue > 520) {
+    navbar.classList.remove('transparent-navbar');
+    navbar.classList.add('white-navbar');
+  } else {
+    navbar.classList.remove('white-navbar');
+    navbar.classList.add('transparent-navbar');
+  }
+}
+
+window.addEventListener('scroll', changeBg);
