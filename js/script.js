444e06a44e38faf2090e94a8d1e49da474550420
Thomas Castaldi <tomas2mars@hotmail.fr>
2022-06-06 17:12:09 +0200

    adding animations

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-//BACKGROUND COLOR CHANGE
+// BACKGROUND COLOR CHANGE //
 
 function changeBg() {
   var navbar = document.getElementById('nav');
@@ -14,12 +14,59 @@ function changeBg() {
 
 window.addEventListener('scroll', changeBg);
 
-//RESPONSIVE NAVBAR
+// RESPONSIVE NAVBAR //
+const links = document.getElementById('links');
+
+links.addEventListener('click', navbarResponsive);
+
 function navbarResponsive() {
-  var navbarMenu = document.getElementById('menu-content');
+  const navbarMenu = document.getElementById('menu-content');
   if (navbarMenu.className === 'menu-content') {
     navbarMenu.className += ' responsive';
   } else {
     navbarMenu.className = 'menu-content';
   }
 }
+
+// ANIMATIONS //
+
+const scrollElements = document.querySelectorAll('.js-scroll');
+
+const elementInView = (el, dividend = 1) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop <=
+    (window.innerHeight || document.documentElement.clientHeight) / dividend
+  );
+};
+
+const elementOutofView = (el) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop > (window.innerHeight || document.documentElement.clientHeight)
+  );
+};
+
+const displayScrollElement = (element) => {
+  element.classList.add('scrolled');
+};
+
+const hideScrollElement = (element) => {
+  element.classList.remove('scrolled');
+};
+
+const handleScrollAnimation = () => {
+  scrollElements.forEach((el) => {
+    if (elementInView(el, 1.25)) {
+      displayScrollElement(el);
+    } else if (elementOutofView(el)) {
+      hideScrollElement(el);
+    }
+  });
+};
+
+window.addEventListener('scroll', () => {
+  handleScrollAnimation();
+});
